3416ab75fa5691ef6f9dbb1cac7bdb397f348421
lora4739 <lora5780@gmail.com>
2020-09-15 20:45:40 -0400

    Added banking project

---
+++ Banking Application/src/com/Banking/ScannerTest.java
@@ -0,0 +1,22 @@
+package com.Banking;
+
+import java.util.Scanner;
+
+public class ScannerTest {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter a string ");
+		//int a=sc.nextInt();
+		//System.out.println(a);
+		 char c= sc.next().charAt(2);
+		 System.out.println("value is :" +c);
+		
+		
+		
+		
+
+	}
+
+}
